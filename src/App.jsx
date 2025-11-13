b88132cfc557cb4aeee399c0be676fe946c3d583
Sri Shakthi Priya Srisailam <sri.srisailam@rootquotient.com>
2025-11-13 13:28:56 +0530

    home page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./modules/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/modules/Home/index.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className='title'>
+        <h1 >SelliQ</h1>
+        <p>Supercharge your sales team <span>with AI</span></p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
